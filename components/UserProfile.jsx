a02ce1a5efbfa90ac66cb0fd160b45bdb5787a90
Eve Cho <e.young.echo@gmail.com>
2022-10-08 18:16:35 -0400

    componentized and separated viewing/editing user profile

--- components/UserProfile.jsx
+++ components/UserProfile.jsx
@@ -1,15 +1,18 @@
 import { useState, useEffect } from 'react';
 import supabase from '../config/supabaseClient';
+import ViewUserProfile from './ViewUserProfile';
 
 // Account is the user's credentials
 export default function UserProfile({ session }) {
   const [loading, setLoading] = useState(true);
+  const [editing, setEditing] = useState(false);  // state that determines whether user is viewing or editing their info
   const [firstname, setFirstname] = useState(null);
   const [lastname, setLastname] = useState(null);
   const [about, setAbout] = useState(null);
   const [age, setAge] = useState(null);
   const [gender, setGender] = useState(null);
   const [id, setId] = useState(null);
+  const [userData, setUserData] = useState(null);
 
   useEffect(() => {
     getProfile();
@@ -21,6 +24,10 @@ export default function UserProfile({ session }) {
     }
     const user = supabase.auth.user();
     return user;
+  }
+
+  const toggleEdit = (e) => {
+    setEditing(!editing);
   }
 
   async function getProfile() {
@@ -37,6 +44,7 @@ export default function UserProfile({ session }) {
         console.log(error);
       }
       if (data) {
+        setUserData(data);
         console.log(data);
         setFirstname(data.firstname);
         setLastname(data.lastname);
@@ -83,73 +91,77 @@ export default function UserProfile({ session }) {
 
   return (
     <div className="form-widget">
-      <div>
-        <label htmlFor="firstname">first name</label>
-        <input
-          id="firstname"
-          type="text"
-          value={firstname || ''}
-          onChange={(e) => setFirstname(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="lastname">last name</label>
-        <input
-          id="lastname"
-          type="text"
-          value={lastname || ''}
-          onChange={(e) => setLastname(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="about">profile bio</label>
-        <input
-          id="about"
-          type="text"
-          value={about || ''}
-          onChange={(e) => setAbout(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="age">age</label>
-        <input
-          id="age"
-          type="text"
-          value={age || ''}
-          onChange={(e) => setAge(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="gender">gender</label>
-        <select value={gender || 'unselected'} onChange={(e) => setGender(e.target.value)}>
-          <option disabled value='unselected'>Select</option>
-          <option value='male'>Male</option>
-          <option value='female'>Female</option>
-          <option value='nb'>Non-binary</option>
-          <option value='other'>Other</option>
-        </select>
-        {/* <input
-          id="gender"
-          type="text"
-          value={age || ''}
-          onChange={(e) => setAge(e.target.value)}
-        /> */}
-      </div>
-      <div>
-        <button
-          className="button primary block"
-          onClick={() => updateProfile({ firstname, lastname })}
-          disabled={loading}
-        >
-          {loading ? 'Loading ...' : 'Update'}
-        </button>
-      </div>
-
+      <button className="button block" onClick={() => toggleEdit()}>{editing ? 'Cancel editing' : 'Edit profile'}</button>
+      {editing ? 
+      (<div>
+        <div>
+          <label htmlFor="firstname">first name</label>
+          <input
+            id="firstname"
+            type="text"
+            value={firstname || ''}
+            onChange={(e) => setFirstname(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="lastname">last name</label>
+          <input
+            id="lastname"
+            type="text"
+            value={lastname || ''}
+            onChange={(e) => setLastname(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="about">profile bio</label>
+          <input
+            id="about"
+            type="text"
+            value={about || ''}
+            onChange={(e) => setAbout(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="age">age</label>
+          <input
+            id="age"
+            type="text"
+            value={age || ''}
+            onChange={(e) => setAge(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="gender">gender</label>
+          <select value={gender || 'unselected'} onChange={(e) => setGender(e.target.value)}>
+            <option disabled value='unselected'>Select</option>
+            <option value='male'>Male</option>
+            <option value='female'>Female</option>
+            <option value='nb'>Non-binary</option>
+            <option value='other'>Other</option>
+          </select>
+          {/* <input
+            id="gender"
+            type="text"
+            value={age || ''}
+            onChange={(e) => setAge(e.target.value)}
+          /> */}
+        </div>
+        <div>
+          <button
+            className="button primary block"
+            onClick={() => updateProfile({ firstname, lastname })}
+            disabled={loading}
+          >
+            {loading ? 'Loading ...' : 'Update'}
+          </button>
+        </div>
+      </div> )
+      : <ViewUserProfile user={userData}/>}
       <div>
         <button
           className="button block"
           onClick={() => supabase.auth.signOut()}
-        >
+          >
           Sign Out
         </button>
       </div>
---
+++ components/ViewUserProfile.jsx
@@ -0,0 +1,8 @@
+// component of UserProfile meant to ONLY VIEW their info, not editing
+export default function ViewUserProfile(user) {
+    return (
+        <div>
+            viewing profile now
+        </div>
+    )
+}
