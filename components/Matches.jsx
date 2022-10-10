ef0b45c1faaf755fe27de8942bda078b268e0fc3
Zach Byerley <zachbyerley@gmail.com>
2022-10-10 11:40:49 -0400

    feat(component/matches): displays dummy list of matches

--- components/Matches.jsx
+++ components/Matches.jsx
@@ -1,12 +1,36 @@
 import React from 'react';
 
+const dummy = [
+  {
+    id: 1,
+    avatar_url:
+      'https://static.wikia.nocookie.net/suitelife/images/8/85/London_Tipton_3.jpg/revision/latest?cb=20110505214610',
+    firstname: 'London',
+    lastname: 'Tipton',
+  },
+  {
+    id: 2,
+    avatar_url:
+      'https://static.wikia.nocookie.net/suitelife/images/8/85/London_Tipton_3.jpg/revision/latest?cb=20110505214610',
+    firstname: 'Zach',
+    lastname: 'Efron',
+  },
+];
+
 export default function Matches() {
   return (
     <div className="match-list">
       <ul>
-        <li>MATCH ONE</li>
-        <li>MATCH TWO</li>
-        {/* each li here will be a small user card that links to /messages/userID */}
+        {dummy.map((user) => (
+          <li key={user.id}>
+            <img src={user.avatar_url} alt="user profile image" />
+            <p>
+              {user.firstname}
+              <br />
+              {user.lastname}
+            </p>
+          </li>
+        ))}
       </ul>
     </div>
   );
