6a86db4e322a8845e0a27a62940fa1d4bf9a5433
Zach Byerley <zachbyerley@gmail.com>
2022-10-10 13:07:57 -0400

    feat(pages/messages): list item now links to messages/userID

--- components/Matches.jsx
+++ components/Matches.jsx
@@ -1,5 +1,6 @@
-import { useSelector, useDispatch } from 'react-redux';
+import Link from 'next/link';
 import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { getAllUserMatches } from '../store/reducers/matchesSlice';
 
 export default function Matches() {
@@ -15,19 +16,23 @@ export default function Matches() {
     <div className="match-list">
       <ul>
         {matches.map((user) => (
-          <li key={user.id}>
-            <img
-              src={user.avatar_url}
-              alt="user profile image"
-              height="100"
-              width="100"
-            />
-            <p>
-              {user.firstname}
-              <br />
-              {user.lastname}
-            </p>
-          </li>
+          <Link href={`/messages/${user.id}`} key={user.id}>
+            <a>
+              <li>
+                <img
+                  src={user.avatar_url}
+                  alt="user profile image"
+                  height="100"
+                  width="100"
+                />
+                <p>
+                  {user.firstname}
+                  <br />
+                  {user.lastname}
+                </p>
+              </li>
+            </a>
+          </Link>
         ))}
       </ul>
     </div>
