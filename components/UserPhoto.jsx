d9c174bb9ed90220b9914a17fb96c1971896225c
Eve Cho <e.young.echo@gmail.com>
2022-10-13 12:41:27 -0400

    UserPhoto: props and handler function tentatively stubbed out

--- components/UserPhoto.jsx
+++ components/UserPhoto.jsx
@@ -1,3 +1,20 @@
-export default function UserPhoto({imgData}) {
-    return (<>some div that contains img functionality</>)
+import { useState } from 'react';
+
+export default function UserPhoto({imgData, userData, setUserData}) {
+    // imgData should contain both the index and URL of the image
+    const handleDeletePhoto = () => {
+        let tempPhotos = userData.user_photos; // this SHOULD be an array
+        tempPhotos.splice(imgData.index, 1);   // this should remove the img from the array
+        setUserData({...userData, user_photos: tempPhotos});
+
+        // OR...
+        // setUserData({...userData, user_photos: [...userData.user_photos].splice(imgData.index, 1)})
+    }
+
+    return (
+        <div className='user_photo'>
+            <button onClick={handleDeletePhoto}>X</button>
+            <img src={imgData.imgURL} alt="image from user's profile data"/>
+        </div>
+    )
 }
