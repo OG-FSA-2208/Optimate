e2ff883e67a4d14b1cd102a598347a44037e6a36
Corum Gold <goldcorum@gmail.com>
2022-10-13 12:18:19 +0200

    Change oauth links to logos

--- components/OAuthBar.jsx
+++ components/OAuthBar.jsx
@@ -1,5 +1,6 @@
 import { useDispatch } from 'react-redux';
 import { providerOAuth } from '../store/reducers/userSlice';
+import { FaGithub, FaFacebook, FaGoogle } from 'react-icons/fa';
 
 export default function OAuthBar() {
   const dispatch = useDispatch();
@@ -9,13 +10,13 @@ export default function OAuthBar() {
   return (
     <div className="auth-buttons">
       <button value="google" onClick={(e) => AttemptProviderOAuth(e)}>
-        google
+        <FaGoogle />
       </button>
       <button value="facebook" onClick={(e) => AttemptProviderOAuth(e)}>
-        facebook
+        <FaFacebook />
       </button>
       <button value="github" onClick={(e) => AttemptProviderOAuth(e)}>
-        github
+        <FaGithub />
       </button>
     </div>
   );
