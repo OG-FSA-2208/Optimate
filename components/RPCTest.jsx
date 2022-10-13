1ca348f0e294d13b25e43b599849c3fbde06bb14
codyWSL <adwingsx@aim.com>
2022-10-13 18:58:00 -0400

    more test

--- components/RPCTest.jsx
+++ components/RPCTest.jsx
@@ -4,8 +4,11 @@ import { logoutUser } from '../store/reducers/userSlice';
 export default function RPCTest() {
   // const router = useRouter();
   const rpcfunction = async () => {
+    const res = await supabase.rpc('testfunc');
     const { data, error } = await supabase.rpc('testfunc');
     console.log('rpc ');
+    console.log(res);
+
     console.log(data);
     console.log(error);
   };
