cfc6262eac23fa5e3b7aa1fb9a4b94855bbc4fed
Eve Cho <e.young.echo@gmail.com>
2022-10-14 18:13:28 -0400

    icons added to userProfile, need to adjust icon styling

--- components/UserProfile.jsx
+++ components/UserProfile.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import UserProfileView from './UserProfileView';
 import UserProfileEdit from './UserProfileEdit';
+import EditIcon from '@mui/icons-material/Edit';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
 // this component only toggles between displaying the view/edit components for UserProfile
 export default function UserProfile({ session }) {
@@ -13,7 +15,7 @@ export default function UserProfile({ session }) {
   return (
     <div className="form-widget">
       <h1>{editing ? 'Editing Profile' : 'Your Profile'}
-      <button className="button block" onClick={() => toggleEdit()}>{editing ? 'Return to profile' : 'Edit profile'}</button>
+      <button className="button block" id='editToggle' onClick={() => toggleEdit()}>{editing ? <><ArrowBackIcon/> Return to Profile View</> : <>Edit Profile <EditIcon/></>}</button>
       </h1>
       <hr/>
       {editing ? <UserProfileEdit/> : <UserProfileView/>}
