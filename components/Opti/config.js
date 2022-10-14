cbbb752c9be63031853a0b692fff2fa93df0db27
Winnie Lau <thinkingoutlau@gmail.com>
2022-10-14 16:02:48 -0400

    bot is working

---
+++ components/Opti/config.js
@@ -0,0 +1,9 @@
+import { createChatBotMessage } from 'react-chatbot-kit';
+
+const config = {
+  initialMessages: [
+    createChatBotMessage(`Hello there gorgeous ;) how can I help you today?`),
+  ],
+};
+
+export default config;
