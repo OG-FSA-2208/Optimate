fd9a373eb7b2823f094daac34029cea8b473452d
Eve Cho <e.young.echo@gmail.com>
2022-10-14 17:07:30 -0400

    age range selectors handle 18 minimum and max cannot go lower than min

--- components/UserProfileEdit.jsx
+++ components/UserProfileEdit.jsx
@@ -261,8 +261,10 @@ export default function EditUserProfile({ session }) {
           <h2>Partner Preferences</h2>
           <div>
             <label htmlFor='wantedAge'>Age range:</label>
-            <input name='wantedAge' type="number" value={userData.ageMin} onChange={e => setUserData({...userData, ageMin: e.target.value})}/>
-            <input name='wantedAge' type="number" value={userData.ageMax} onChange={e => setUserData({...userData, ageMax: e.target.value})}/>
+            <span>From </span>
+            <input name='wantedAge' type="number" value={userData.ageMin || userData.age - 1} onChange={e => e.target.value < 18 ? alert('Too low. Please choose an age 18 or above') : setUserData({...userData, ageMin: e.target.value})}/>
+            <span> to </span>
+            <input name='wantedAge' type="number" value={userData.ageMax || userData.age + 1} onChange={e => e.target.value < userData.ageMin ? alert('Too low. Please choose an age above your selected minimum') : setUserData({...userData, ageMax: e.target.value})}/>
           <div>
             <label htmlFor='wanted-age'>Age range:</label>
             <input id='wanted-age' type='text' value={userData.wantAge || ''} onChange={(e) => setUserData({...userData, wantAge: e.target.value})}/>
@@ -280,6 +282,7 @@ export default function EditUserProfile({ session }) {
           </div>
         </div>
       </div>
+      </div>
       {/* BELOW DIV IS THE BUTTON TO UPDATE ALL THE userData NEWLY SET */}
       <div>
         <button
@@ -290,7 +293,6 @@ export default function EditUserProfile({ session }) {
           {loading ? 'Loading ...' : 'Save Profile'}
         </button>
         <div>{updated ? 'Profile updated' : ''}</div>
-      </div>
       </div>
     </div>
   );
