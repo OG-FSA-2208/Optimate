600f0565353318b89ea12b3d85b2ead709b1bfa7
codyWSL <adwingsx@aim.com>
2022-10-16 17:50:45 -0400

    fix

--- components/Opti/Opti.jsx
+++ components/Opti/Opti.jsx
@@ -1,7 +1,7 @@
 import Chatbot from 'react-chatbot-kit';
 import 'react-chatbot-kit/build/main.css';
 import config from './config.js';
-import MessageParser from './Messageparser.jsx';
+import MessageParser from './MessageParser.jsx';
 import ActionProvider from './ActionProvider.jsx';
 
 const MyComponent = () => {
