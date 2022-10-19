658de1768d2365a6596890b8d85b30f155fe0678
Eve Cho <e.young.echo@gmail.com>
2022-10-19 13:22:02 -0400

    fleshing out userprofileview

--- components/UserPhoto.jsx
+++ components/UserPhoto.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import DeleteIcon from '@mui/icons-material/Delete'
 
-export default function UserPhoto({imgData, userData, setUserData}) {
+export default function UserPhoto({imgData, userData, setUserData, onView}) {
     // imgData should contain both the index and URL of the image
     const handleDeletePhoto = () => {
         let tempPhotos = [...userData.user_photos]; // this SHOULD be an array
@@ -11,7 +11,7 @@ export default function UserPhoto({imgData, userData, setUserData}) {
 
     return (
         <div className='user_photo'>
-            <button onClick={handleDeletePhoto}><DeleteIcon/></button>
+            {onView ? <></> : <button onClick={handleDeletePhoto}><DeleteIcon/></button>}
             <img src={imgData.imgURL} alt="image from user's profile data"/>
         </div>
     )
--- components/UserProfileView.jsx
+++ components/UserProfileView.jsx
@@ -5,6 +5,7 @@ import { logoutUser } from '../store/reducers/userSlice';
 import { getLoggedInUser, getUser } from '../store/reducers/profileSlice';
 import { useSelector } from 'react-redux';
 import supabase from '../config/supabaseClient';
+import UserPhoto from './UserPhoto';
 
 // component of UserProfile meant to ONLY VIEW their info, not editing
 export default function ViewUserProfile() {
@@ -51,23 +52,232 @@ export default function ViewUserProfile() {
         'Loading Profile...'
       ) : (
         <div>
-          <button onClick={updateUserLocation}>Update Location</button>
-          <img src={userData?.avatar_url} height="250px" />
-          <p>
-            Full name: {userData?.firstname || '(no name)'} {userData?.lastname}
-          </p>
-          <p>Age: {userData?.age || '(no age)'}</p>
-          <p>Gender: {userData?.gender || 'unselected'}</p>
-          <p>About: {userData?.about}</p>
-          <p>Location: {userData?.location}</p>
-          <p>Occupation: {userData?.occupation}</p>
-          <p>Smokes? {userData?.smoker ? 'Yes' : 'No'}</p>
-          <p>Drinks? {userData?.drinker ? 'Yes' : 'No'}</p>
-          <p>Priority: {userData?.priority}</p>
-          <p>Love Language (giving): {userData?.loveLangGiving}</p>
-          <p>Love Language (receiving): {userData?.loveLangReceiving}</p>
-          <p>Your Interests:</p>
-          <p>{userData?.user_interests?.map((tag) => tag.label).join(', ')}</p>
+          <div id="profileEditAvatar">
+            <div id="avatarSection">
+              <img src={userData?.avatar_url || ''} />
+            </div>
+            <div>
+              <h2>{(userData?.firstname || '') + ' ' + (userData?.lastname || '')}</h2>
+              <div>
+                <h3>About You</h3>
+                <p>{userData?.about || ''}</p>
+              </div>
+              <div>
+                <h3>Age</h3>
+                <p>{userData?.age || ''}</p>
+              </div>
+              <div>
+                <h3>Gender</h3>
+                <p>{userData?.gender || ''}</p>
+              </div>
+              <div>
+                <h3>Location</h3>
+                <p>{userData?.location || ''}</p>
+                <button onClick={updateUserLocation}>Update Location</button>
+              </div>
+            </div>
+          </div>
+          <hr/>
+          <div id='optionalPhotoUploads'>
+            {userData?.user_photos?.map((photoURL, ind) =>
+            <UserPhoto key={ind} imgData={{imgURL: photoURL, index: ind}}
+            userData={userData} onView={true}/>)}
+          </div>
+          <hr/>
+          <div className="profile-info">
+            <div className="profile-user">
+              <div>
+                <h3>Occupation</h3>
+                <p>{userData?.occupation || ''}</p>
+              </div>
+            <div>
+              <h3>Smokes</h3>
+              <p>{userData?.smoker ? 'Yes, I smoke' : `No, I don't smoke at all`}</p>
+            </div>
+          <div>
+            <h3>Drinks Alcohol</h3>
+            <p>{userData?.drinker ? 'Yes, I drink alcohol' : `No, I don't drink alcohol at all`}</p>
+          </div>
+          {/* this is the select-dropdown-checkbox for interests!!!! */}
+          <div>
+            <h3>User Interests:</h3>
+            <ul>{userData?.user_interests?.length > 0 ? 
+                userData?.user_interests?.map(interest => <li key={interest.value}>{interest.label}</li>)
+                : 'This user has no interests'}</ul>
+          </div>
+          <div>
+            <h3>Your love language (giving)</h3>
+            <p>{userData?.loveLangGiving || 'You have not selected any yet'}</p>
+          </div>
+          <div>
+            <h3>Your love language (receiving)</h3>
+            <p>{userData?.loveLangReceiving || 'You have not selected any yet'}</p>
+          </div>
+          <div>
+            <h3>Your top priority</h3>
+            <p>{userData?.priority || 'You currently do not have any priority'}</p>
+          </div>
+        </div>
+        {/* THIS IS WHERE A USER'S PARTNER PREFERENCES GO */}
+        <div className="profile-preference">
+          <h2>Partner Preferences</h2>
+          <div>
+            <h3 htmlFor="wantedAge">Age range:</h3>
+            <span>From </span>
+            <input
+              name="wantedAge"
+              type="number"
+              value={userData?.ageMin || userData?.age - 1}
+              onChange={(e) => setUserData({ ...userData, ageMin: e.target.value })
+              }
+            />
+            <span> to </span>
+            <input
+              name="wantedAge"
+              type="number"
+              value={userData?.ageMax || userData?.age + 1}
+              onChange={(e) => setUserData({ ...userData, ageMax: e.target.value })
+              }
+            />
+          </div>
+          <div>
+            <h3 htmlFor="wanted-gender">Preferred gender</h3>
+            <select
+              value={
+                userData?.genderPreference ? userData?.genderPreference : 'null'
+              }
+              onChange={(e) =>
+                e.target.value !== 'null'
+                  ? setUserData({
+                      ...userData,
+                      genderPreference: e.target.value,
+                    })
+                  : setUserData({ ...userData, genderPreference: null })
+              }
+            >
+              <option value="Male">Male</option>
+              <option value="Female">Female</option>
+              <option value="Non-binary/Genderfluid">
+                Non-binary/Genderfluid
+              </option>
+              <option value="null">No Preference</option>
+              <option value="Other">Other</option>
+            </select>
+          </div>
+          <div>
+            <h3>Smokes?</h3>
+            <input
+              value={true}
+              checked={userData?.smokingPreference}
+              type="radio"
+              name="smokerPref"
+              onChange={(e) =>
+                setUserData({ ...userData, smokingPreference: true })
+              }
+            />{' '}
+            Yes, I want them to be a smoker
+            <br />
+            <input
+              value={false}
+              checked={
+                typeof userData?.smokingPreference === 'boolean' &&
+                !userData?.smokingPreference
+              }
+              type="radio"
+              name="smokerPref"
+              onChange={(e) =>
+                setUserData({ ...userData, smokingPreference: false })
+              }
+            />{' '}
+            No, I don't want them to be a smoker
+            <br />
+            <input
+              value={undefined}
+              checked={userData?.smokingPreference === null}
+              type="radio"
+              name="smokerPref"
+              onChange={(e) =>
+                setUserData({ ...userData, smokingPreference: null })
+              }
+            />{' '}
+            No real preference
+          </div>
+          <div>
+            <h3>Drinks?</h3>
+            <input
+              value={true}
+              checked={userData?.drinkingPreference}
+              type="radio"
+              name="drinkingPref"
+              onChange={(e) =>
+                setUserData({ ...userData, drinkingPreference: true })
+              }
+            />{' '}
+            Yes, I want them to drink alcohol
+            <br />
+            <input
+              value={false}
+              checked={
+                typeof userData?.drinkingPreference === 'boolean' &&
+                !userData?.drinkingPreference
+              }
+              type="radio"
+              name="drinkingPref"
+              onChange={(e) =>
+                setUserData({ ...userData, drinkingPreference: false })
+              }
+            />{' '}
+            No, I don't want them to drink any alcohol
+            <br />
+            <input
+              value={undefined}
+              checked={userData?.drinkingPreference === null}
+              type="radio"
+              name="drinkingPref"
+              onChange={(e) =>
+                setUserData({ ...userData, drinkingPreference: null })
+              }
+            />{' '}
+            No real preference
+          </div>
+          <div>
+            <h3 htmlFor="partner-priority">Prioritizes</h3>
+            <select
+              value={userData?.priorityPreference || 'unselected'}
+              onChange={(e) =>
+                setUserData({ ...userData, priorityPreference: e.target.value })
+              }
+            >
+              <option value="unselected">Unsure/Don't Care</option>
+              <option value="Family">Family</option>
+              <option value="Friends">Friends</option>
+              <option value="Children">Children</option>
+              <option value="Work">Work/Job</option>
+              <option value="Self-care">Selfcare</option>
+            </select>
+          </div>
+          <div>
+            <h3>Match by Love Languages?</h3>
+            <input
+              value={true}
+              checked={userData?.matchByLL}
+              type="radio"
+              name="matchLL"
+              onChange={(e) => setUserData({ ...userData, matchByLL: true })}
+            />{' '}
+            Yes, this is important to me
+            <br />
+            <input
+              value={false}
+              checked={!userData?.matchByLL}
+              type="radio"
+              name="matchLL"
+              onChange={(e) => setUserData({ ...userData, matchByLL: false })}
+            />{' '}
+            No, I don't mind a mismatch
+          </div>
+        </div>
+      </div>
         </div>
       )}
     </div>
