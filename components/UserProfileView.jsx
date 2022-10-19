99204399198eaac2e6d9703fc4ef3ece269d6691
Eve Cho <e.young.echo@gmail.com>
2022-10-19 15:17:21 -0400

    all fields added to userprofileview

--- components/UserProfileView.jsx
+++ components/UserProfileView.jsx
@@ -87,195 +87,67 @@ export default function ViewUserProfile() {
           <hr/>
           <div className="profile-info">
             <div className="profile-user">
+              <h2>Your Details</h2>
               <div>
                 <h3>Occupation</h3>
                 <p>{userData?.occupation || 'Unemployed'}</p>
               </div>
-            <div>
-              <h3>Smokes</h3>
-              <p>{userData?.smoker ? 'Yes, I smoke' : `No, I don't smoke at all`}</p>
+              <div>
+                <h3>Smokes</h3>
+                <p>{userData?.smoker ? 'Yes, I smoke' : `No, I don't smoke at all`}</p>
+              </div>
+              <div>
+                <h3>Drinks Alcohol</h3>
+                <p>{userData?.drinker ? 'Yes, I drink alcohol' : `No, I don't drink alcohol at all`}</p>
+              </div>
+              <div>
+                <h3>User Interests:</h3>
+                <ul>{userData?.user_interests?.length > 0 ? 
+                    userData?.user_interests?.map(interest => <li key={interest.value}>{interest.label}</li>)
+                    : 'This user has no interests'}</ul>
+              </div>
+              <div>
+                <h3>Your love language (giving)</h3>
+                <p>{userData?.loveLangGiving || 'You have not selected any yet'}</p>
+              </div>
+              <div>
+                <h3>Your love language (receiving)</h3>
+                <p>{userData?.loveLangReceiving || 'You have not selected any yet'}</p>
+              </div>
+              <div>
+                <h3>Your top priority</h3>
+                <p>{userData?.priority || 'You currently do not have any priority'}</p>
+              </div>
             </div>
+            <div className="profile-preference">
+              <h2>Partner Preferences</h2>
           <div>
-            <h3>Drinks Alcohol</h3>
-            <p>{userData?.drinker ? 'Yes, I drink alcohol' : `No, I don't drink alcohol at all`}</p>
-          </div>
-          {/* this is the select-dropdown-checkbox for interests!!!! */}
-          <div>
-            <h3>User Interests:</h3>
-            <ul>{userData?.user_interests?.length > 0 ? 
-                userData?.user_interests?.map(interest => <li key={interest.value}>{interest.label}</li>)
-                : 'This user has no interests'}</ul>
-          </div>
-          <div>
-            <h3>Your love language (giving)</h3>
-            <p>{userData?.loveLangGiving || 'You have not selected any yet'}</p>
-          </div>
-          <div>
-            <h3>Your love language (receiving)</h3>
-            <p>{userData?.loveLangReceiving || 'You have not selected any yet'}</p>
-          </div>
-          <div>
-            <h3>Your top priority</h3>
-            <p>{userData?.priority || 'You currently do not have any priority'}</p>
-          </div>
-        </div>
-        {/* THIS IS WHERE A USER'S PARTNER PREFERENCES GO */}
-        <div className="profile-preference">
-          <h2>Partner Preferences</h2>
-          <div>
-            <h3 htmlFor="wantedAge">Age range:</h3>
-            <span>From </span>
-            <input
-              name="wantedAge"
-              type="number"
-              value={userData?.ageMin || userData?.age - 1}
-              onChange={(e) => setUserData({ ...userData, ageMin: e.target.value })
-              }
-            />
-            <span> to </span>
-            <input
-              name="wantedAge"
-              type="number"
-              value={userData?.ageMax || userData?.age + 1}
-              onChange={(e) => setUserData({ ...userData, ageMax: e.target.value })
-              }
-            />
+            <h3>Looking for ages:</h3>
+            <p>{userData.ageMin ? userData.ageMin : 'N/A'} - {userData.ageMax ? userData.ageMax : 'N/A'}</p>
           </div>
           <div>
             <h3 htmlFor="wanted-gender">Preferred gender</h3>
-            <select
-              value={
-                userData?.genderPreference ? userData?.genderPreference : 'null'
-              }
-              onChange={(e) =>
-                e.target.value !== 'null'
-                  ? setUserData({
-                      ...userData,
-                      genderPreference: e.target.value,
-                    })
-                  : setUserData({ ...userData, genderPreference: null })
-              }
-            >
-              <option value="Male">Male</option>
-              <option value="Female">Female</option>
-              <option value="Non-binary/Genderfluid">
-                Non-binary/Genderfluid
-              </option>
-              <option value="null">No Preference</option>
-              <option value="Other">Other</option>
-            </select>
+            <p>{userData?.genderPreference ? userData.genderPreference : 'No gender preference'}</p>
           </div>
           <div>
             <h3>Smokes?</h3>
-            <input
-              value={true}
-              checked={userData?.smokingPreference}
-              type="radio"
-              name="smokerPref"
-              onChange={(e) =>
-                setUserData({ ...userData, smokingPreference: true })
-              }
-            />{' '}
-            Yes, I want them to be a smoker
-            <br />
-            <input
-              value={false}
-              checked={
-                typeof userData?.smokingPreference === 'boolean' &&
-                !userData?.smokingPreference
-              }
-              type="radio"
-              name="smokerPref"
-              onChange={(e) =>
-                setUserData({ ...userData, smokingPreference: false })
-              }
-            />{' '}
-            No, I don't want them to be a smoker
-            <br />
-            <input
-              value={undefined}
-              checked={userData?.smokingPreference === null}
-              type="radio"
-              name="smokerPref"
-              onChange={(e) =>
-                setUserData({ ...userData, smokingPreference: null })
-              }
-            />{' '}
-            No real preference
+            <p>{typeof userData?.smokingPreference !== 'boolean' ? 'No real preference'
+              : userData?.smokingPreference ? 'Yes, I want them to be a smoker' : `No, I don't want them to be a smoker`}
+            </p>
           </div>
           <div>
             <h3>Drinks?</h3>
-            <input
-              value={true}
-              checked={userData?.drinkingPreference}
-              type="radio"
-              name="drinkingPref"
-              onChange={(e) =>
-                setUserData({ ...userData, drinkingPreference: true })
-              }
-            />{' '}
-            Yes, I want them to drink alcohol
-            <br />
-            <input
-              value={false}
-              checked={
-                typeof userData?.drinkingPreference === 'boolean' &&
-                !userData?.drinkingPreference
-              }
-              type="radio"
-              name="drinkingPref"
-              onChange={(e) =>
-                setUserData({ ...userData, drinkingPreference: false })
-              }
-            />{' '}
-            No, I don't want them to drink any alcohol
-            <br />
-            <input
-              value={undefined}
-              checked={userData?.drinkingPreference === null}
-              type="radio"
-              name="drinkingPref"
-              onChange={(e) =>
-                setUserData({ ...userData, drinkingPreference: null })
-              }
-            />{' '}
-            No real preference
+            <p>{typeof userData?.drinkingPreference !== 'boolean' ? 'No real preference'
+              : userData?.drinkingPreference ? 'Yes, I want them to be a drinker' : `No, I don't want them to be a drinker`}
+            </p>
           </div>
           <div>
-            <h3 htmlFor="partner-priority">Prioritizes</h3>
-            <select
-              value={userData?.priorityPreference || 'unselected'}
-              onChange={(e) =>
-                setUserData({ ...userData, priorityPreference: e.target.value })
-              }
-            >
-              <option value="unselected">Unsure/Don't Care</option>
-              <option value="Family">Family</option>
-              <option value="Friends">Friends</option>
-              <option value="Children">Children</option>
-              <option value="Work">Work/Job</option>
-              <option value="Self-care">Selfcare</option>
-            </select>
+            <h3>Prioritizes</h3>
+            <p>{userData?.priorityPreference || 'Partner priority is unimportant to me'}</p>
           </div>
           <div>
             <h3>Match by Love Languages?</h3>
-            <input
-              value={true}
-              checked={userData?.matchByLL}
-              type="radio"
-              name="matchLL"
-              onChange={(e) => setUserData({ ...userData, matchByLL: true })}
-            />{' '}
-            Yes, this is important to me
-            <br />
-            <input
-              value={false}
-              checked={!userData?.matchByLL}
-              type="radio"
-              name="matchLL"
-              onChange={(e) => setUserData({ ...userData, matchByLL: false })}
-            />{' '}
-            No, I don't mind a mismatch
+            <p>{userData?.matchByLL ? 'Yes, this is important to me' : `No, I don't mind a mismatch`}</p>
           </div>
         </div>
       </div>
