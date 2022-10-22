4d8c3a0f15e0e515e6a5d72e05db09542eb62859
Eve Cho <e.young.echo@gmail.com>
2022-10-22 15:54:29 -0400

    tentative deleteUser function created

---
+++ supabase/api/deleteUser.js
@@ -0,0 +1,9 @@
+import supabase from "../../config/supabaseClient";
+
+export const deleteUser = async (userId) => {
+    const { data, user, error } = await supabase.auth.api.deleteUser(userId);
+    if (error) {
+        return `some error occurred when trying to delete the user: ${error}`;
+    }
+    return data;
+}
