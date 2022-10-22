91efd0b8c5d34247c88aaae5de41e0106fefdb27
Eve Cho <e.young.echo@gmail.com>
2022-10-22 18:56:00 -0400

    deleteUser works... i think

--- supabase/api/deleteUser.js
+++ supabase/api/deleteUser.js
@@ -1,14 +1,35 @@
-import supabase from "../../config/supabaseClient";
+import { createClient } from '@supabase/supabase-js';
+// creating a client just for admin access
+// NO OTHER PART OF THE APP SHOULD BE ACCESSING THIS CLIENT!!!
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseServiceKey = process.env.NEXT_PUBLIC_SUPABASE_SERVICE_KEY;
+const supabaseAdmin = createClient(supabaseUrl, supabaseServiceKey);
 
 export const deleteUser = async (userId) => {
     console.dir(userId);
-    // return true;
-    console.dir(supabase.auth.api.deleteUser);
-    const { data, user, error } = await supabase.auth.api.deleteUser(userId);
+    await supabaseAdmin.from('profiles').delete().eq('id', userId);
+    await supabaseAdmin.from('profiles_secure').delete().eq('id', userId);
+    await supabaseAdmin.from('matches').delete().eq('id', userId);
+    await supabaseAdmin.from('matches2').delete().eq('id', userId);
+    await supabaseAdmin.from('messages').delete().or(`from.eq.${userId}, to.eq.${userId}`);
+    // await supabaseAdmin.from('old_messages').delete().match({id:userId});
+    await supabaseAdmin.from('blacklist').delete().eq('id', userId);
+    const filesToRemove = (await supabaseAdmin.storage.from('avatars').list()).data
+        .reduce((prev, file) => {
+            if (file.name.includes(userId)) {
+                prev.push(file.name);
+                return prev;
+            }
+            return prev;
+        }, []);
+    console.dir(filesToRemove);
+    const {data} = await supabaseAdmin.storage.from('avatars').remove(filesToRemove);
+    console.dir(data);
+    const { data: user, error } = await supabaseAdmin.auth.api.deleteUser(userId);
     if (error) {
         return `some error occurred when trying to delete the user: ${error}`;
     }
-    console.dir(data);
-    console.dir(user);
+    // console.dir(user);
     return data;
 }
