0df72f984cc965f19321f20bf5ed7c5a29f8b01d
codyWSL <adwingsx@aim.com>
2022-10-23 11:53:45 -0400

    updated sql functions

--- supabase/time_to_match.sql
+++ supabase/time_to_match.sql
@@ -6,16 +6,36 @@ as $$
 declare
   timer text;
 begin
+
   if (
-        (select AGE(
+        ((select AGE(
             (select (now()::date)+1)
               , 
             (select created_at from matches2 m where id=auth.uid() ORDER BY created_at DESC limit 1)
         ))
         >
-        (SELECT '1 day'::interval)
+        (SELECT '1 day'::interval))
+        or
+        (select created_at from matches2 m where id=auth.uid() limit 1) is null
       )
-      then return '0';
+      then 
+        if (
+              (select AGE(
+                    (select now())
+                      , 
+                    (select attempted_at from lastmatch m where id=auth.uid() ORDER BY attempted_at DESC limit 1)
+              ))
+              <
+              (SELECT '1 minute'::interval)
+            )
+        then return age(  
+                    (select attempted_at + '5 minute' from lastmatch m where id=auth.uid() ORDER BY attempted_at DESC limit 1)
+                      , 
+                    (select now())
+                    );
+        else return '0';
+        end if;
+      
       else   
       return age(  
               (select (now()::date)+1)
